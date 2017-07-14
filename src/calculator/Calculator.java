308f04cc401cf5e38e475f3b01239be27b8e19b3
Your Name <binojishara21@gmail.com>
2017-07-14 13:28:58 +0530

    first commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+
+public class Calculator {
+
+    
+    public static void main(String[] args) {
+       MainFrame mf = new MainFrame();
+       mf.setVisible(true);
+    }
+}
